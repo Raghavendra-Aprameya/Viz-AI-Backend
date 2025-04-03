dbe93e851375e77cc753a0f36ac533098cd57843
Raghavendra-Aprameya <1rn21cs030.aprameya@gmail.com>
2025-04-03 12:31:36 +0530

    feat:Create super user implemented

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,9 +5,12 @@ from app.services.authServices import register_user
 
 router = APIRouter(prefix= "/api/v1/auth")
 
-@router.post("/register",status_code= status.HTTP_201_CREATED)
+@router.post("/register-super-admin",status_code= status.HTTP_201_CREATED,)
 async def register(user: UserModel):
 
     register_user(user)
 
 
+@router.post("/login")
+async def login(login_data: LoginData):
+    pass
--- app/services/authServices.py
+++ app/services/authServices.py
@@ -1,6 +1,35 @@
 from app.models.schema_models import UserModel
+from sqlalchemy.orm import Session
+from app.core.db import get_db
+from fastapi import APIRouter, status, Depends, Response, Request, HTTPException
+from sqlalchemy.exc import IntegrityError
+from pydantic import ValidationError
+# from fastapi.responses import JSONResponse
+import bcrypt
 
-def register_user(user: UserModel):
-    pass
 
-# __all__ = ['register_user']
+async def register_user(user: UserModel, response: Response, db: Session = Depends(get_db)):
+    try:
+      res = await db.query(UserModel).filter(UserModel.username==user.username).first()
+      if res:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="User already exists")
+      else: 
+        # Hash password with bcrypt
+        salt = bcrypt.gensalt()
+        user.password = bcrypt.hashpw(user.password.encode(), salt).decode()
+        new_user = UserModel(**user.model_dump())
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        response.status_code = status.HTTP_201_CREATED
+        return new_user
+
+    except Exception as e:
+      raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=e)
+    
+
+  
+    
+    
+
+
