ec8e3851db3d69125e5cf61cb5e45cbb8570a5d6
Shiv Sahoo <shivshankar.s@webknot.in>
2025-11-14 16:31:32 +0530

    allow sites

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -33,7 +33,7 @@ POOL_RECYCLE = 300
 POOL_SIZE = 5
 MAX_OVERFLOW = 0
 
-ALLOWED_ORIGINS=["http://170.187.237.181:3000"]
+ALLOWED_ORIGINS=["http://170.187.237.181:3000","http://localhost:3000","http://localhost:3001","http://localhost:3002","http://localhost:3003"]
 ALLOWED_CREDENTIALS = True
 ALLOWED_METHODS = ["*"]
 ALLOWED_HEADERS = ["*"]
