166f0162077fc4597ceff5badbc16919fecd3320
Ayan Acharyya <ayan.a@webknot.in>
2026-02-12 16:01:48 +0530

    added versions

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -34,18 +34,14 @@ POOL_SIZE = 20
 MAX_OVERFLOW = 10
 POOL_TIMEOUT = 30
 
-ALLOWED_ORIGINS=["http://170.187.237.181:3000","http://172.105.61.172:3000/","http://localhost:3000","http://localhost:3001","http://localhost:3002","http://localhost:3003", "https://*.ngrok-free.app", "https://*.ngrok.io", "https://*.ngrok-free.dev", "http://192.168.1.62:3000/"]
+ALLOWED_ORIGINS=["http://170.187.237.181:3000","http://localhost:3000","http://localhost:3001","http://localhost:3002","http://localhost:3003", "https://*.ngrok-free.app", "https://*.ngrok.io", "https://*.ngrok-free.dev", "http://192.168.1.62:3000/"]
 ALLOWED_CREDENTIALS = True
 ALLOWED_METHODS = ["*"]
 ALLOWED_HEADERS = ["*", "Authorization"]
 
-#LOCAL
-# LLM_SERVICE_URL = "http://localhost:8001/queries/"
-# LLM_SPREADSHEET_URL = "http://localhost:8001/generate-sheet-queries/"
+LLM_SERVICE_URL = "http://localhost:8001/queries/"
+LLM_SPREADSHEET_URL = "http://localhost:8001/generate-sheet-queries/"
 
-# Use Docker Service Name
-LLM_SERVICE_URL = "http://vizai_llm_service:8001/queries/"
-LLM_SPREADSHEET_URL = "http://vizai_llm_service:8001/generate-sheet-queries/"
 # External database connection pool settings
 EXTERNAL_POOL_SIZE = 5          # Conservative pool size per external DB
 EXTERNAL_MAX_OVERFLOW = 10      # Burst capacity (total: 15 connections)
